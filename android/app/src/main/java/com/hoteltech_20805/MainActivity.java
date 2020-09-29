8acee1705f48abd0308fc92c9a93d8d5661d3802
Crowdbotics <team@crowdbotics.com>
2020-09-29 01:31:45 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/hoteltech_20805/MainActivity.java
@@ -0,0 +1,15 @@
+package com.hoteltech_20805;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "hoteltech_20805";
+    }
+}
